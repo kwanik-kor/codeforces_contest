a374a11cd59c8aca52516a41bc1d5e4a3ecec257
kwanik-kor <rhksdlr134@naver.com>
2020-03-24 15:40:59 +0900

    20200324 15:40 codeforces round #627, Div.3 / E (dp)

--- codeforces/src/Main.java
+++ codeforces/src/Main.java
@@ -8,39 +8,46 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int find(Integer arr[], int t) {
-		int l = 0;
-		int r = arr.length - 1;
-		while(l < r-1) {
-			int mid = (l + r - 1)/2;
-			if(arr[mid] > t)
-				l = mid + 1;
-			else
-				r = mid;
-		}
-		if(arr[l] <= t)
-			return l;
-		else if(arr[r] <= t)
-			return r;
-		return arr.length;
+	static int n, h, l, r;
+	static int[] a;
+	static int[][] dp;//case + time
+	
+	public static int solve(int start, int prev) {
+		if(start == n) 
+			return 0;
+		if(dp[start][prev] != -1)
+			return dp[start][prev];
+		
+		int pos1 = (a[start] + prev)%h;
+		int ans1 = 0;
+		if(pos1 >= l && pos1 <= r) ans1 = 1;
+		int first = ans1 + solve(start + 1, pos1);
+		
+		int pos2 = (a[start] - 1 + h + prev)%h;
+		int ans2 = 0;
+		if(pos2 >= l && pos2 <= r) ans2 = 1;
+		int second = ans2 + solve(start + 1, pos2);
+		dp[start][prev] = Math.max(first, second);
+		
+		return dp[start][prev];
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st1 = new StringTokenizer(br.readLine());
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		Integer arr[] = new Integer[N];
-		for(int i = 0; i<N; i++)
-			arr[i] = Integer.parseInt(st1.nextToken()) - Integer.parseInt(st2.nextToken());
-		long cnt = 0;
-		Arrays.sort(arr, Comparator.reverseOrder());
-		for(int i = 0; i<N; i++) {
-			if(arr[i] <= 0) break;
-			cnt += find(arr, -arr[i]) - (i + 1); 
-		}
-		bw.write(cnt+"");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		a = new int[n];
+		dp = new int[n][h];
+		for(int i = 0; i<n; i++)
+			Arrays.fill(dp[i], -1);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n; i++)
+			a[i] = Integer.parseInt(st.nextToken());
+		bw.write(solve(0, 0) + "");
 		bw.flush();
 		bw.close();
 		br.close();
