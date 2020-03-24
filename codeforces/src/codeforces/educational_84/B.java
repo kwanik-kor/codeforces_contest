66dba224aac6ab895c170ee6d0b7fe5fecd44563
kwanik-kor <rhksdlr134@naver.com>
2020-03-24 10:18:13 +0900

    20200324 10:17 codeforces educational round #84 / B

--- codeforces/src/codeforces/educational_84/B.java
+++ codeforces/src/codeforces/educational_84/B.java
@@ -9,32 +9,49 @@ import java.util.StringTokenizer;
 
 public class B {
 	static int n;
-	static boolean princess[];
-	static boolean prince[];
 	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int testCase = Integer.parseInt(br.readLine());
-		for(int t = 0; t<testCase; t++) {
+		while(testCase-- > 0) {
 			n = Integer.parseInt(br.readLine());
-			prince = new boolean[n+1];
-			princess = new boolean[n+1];
-			for(int i = 1; i<=n; i++) {
-				st = new StringTokenizer(br.readLine());
-				while(st.hasMoreTokens()) {
-					int n = Integer.parseInt(st.nextToken());
-					if(!prince[n]) {
-						princess[i] = true;
-						prince[n] = true;
+			int a[][] = new int[n][];
+			for(int i = 0; i<n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				a[i] = new int[x];
+				for(int j = 0; j<x; j++)
+					a[i][j] = Integer.parseInt(st.nextToken());
+			}
+			boolean taken[] = new boolean[n + 1];
+			boolean rem[] = new boolean[n + 1];
+			for(int i = 0; i<n; i++) {
+				int c = 0;
+				for(int j = 0; j < a[i].length; j++) {
+					if(!taken[a[i][j]]) {
+						c = 1;
+						taken[a[i][j]] = true;
 						break;
 					}
 				}
+				if(c == 0) rem[i] = true;
 			}
-			for(int i = 1; i<=n; i++) {
-				
+			boolean flag = false;
+			for(int i = 0; i<n; i++) {
+				if(rem[i]) {
+					for(int j = 1; j<=n; j++) {
+						if(!taken[j]) {
+							bw.write("IMPROVE\n" + (i + 1) + " " + j + "\n");
+							flag = true;
+							break;
+						}
+					}
+				}
+				if(flag) break;
 			}
+			if(!flag) bw.write("OPTIMAL\n");
 		}
 		bw.flush();
 		bw.close();
