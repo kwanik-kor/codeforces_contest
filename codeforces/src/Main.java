1fb2970ebeee1b110ccbb3e61c692e660b07ae02
kwanik-kor <rhksdlr134@naver.com>
2020-03-24 12:14:48 +0900

    20200324 12:14 codeforces round#627 Div.3

--- codeforces/src/Main.java
+++ codeforces/src/Main.java
@@ -3,42 +3,47 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int find(Integer arr[], int t) {
+		int l = 0;
+		int r = arr.length - 1;
+		while(l < r-1) {
+			int mid = (l + r - 1)/2;
+			if(arr[mid] > t)
+				l = mid + 1;
+			else
+				r = mid;
+		}
+		if(arr[l] <= t)
+			return l;
+		else if(arr[r] <= t)
+			return r;
+		return arr.length;
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int testCase = Integer.parseInt(br.readLine());
-		while(testCase-- > 0) {
-			String str = br.readLine();
-			int n = str.length();
-			char arr[] = new char[n + 2];
-			for(int i = 1; i<=n; i++)
-				arr[i] = str.charAt(i-1);
-			arr[0] = arr[n+1] = 'R';
-			int ans = 1;
-			for(int i = 0; i<=n+1; i++) {
-				int c = 1;
-				if(arr[i] == 'R') 
-					continue;
-				while(i <= n+1) {
-					if(arr[i] != 'R') {
-						c++;
-					}else {
-						ans = Math.max(c, ans);
-						break;
-					}
-					i++;
-				}
-			}
-			bw.write(ans + "\n");
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		Integer arr[] = new Integer[N];
+		for(int i = 0; i<N; i++)
+			arr[i] = Integer.parseInt(st1.nextToken()) - Integer.parseInt(st2.nextToken());
+		long cnt = 0;
+		Arrays.sort(arr, Comparator.reverseOrder());
+		for(int i = 0; i<N; i++) {
+			if(arr[i] <= 0) break;
+			cnt += find(arr, -arr[i]) - (i + 1); 
 		}
+		bw.write(cnt+"");
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-
 
 }
--- codeforces/src/codeforces/round_627/D.java
+++ codeforces/src/codeforces/round_627/D.java
@@ -5,9 +5,27 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class D {
+	static int find(Integer arr[], int t) {
+		int l = 0;
+		int r = arr.length - 1;
+		while(l < r-1) {
+			int mid = (l + r - 1)/2;
+			if(arr[mid] > t)
+				l = mid + 1;
+			else
+				r = mid;
+		}
+		if(arr[l] <= t)
+			return l;
+		else if(arr[r] <= t)
+			return r;
+		return arr.length;
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,14 +33,14 @@ public class D {
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st1 = new StringTokenizer(br.readLine());
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		int arr[] = new int[N];
+		Integer arr[] = new Integer[N];
 		for(int i = 0; i<N; i++)
 			arr[i] = Integer.parseInt(st1.nextToken()) - Integer.parseInt(st2.nextToken());
-		int cnt = 0;
-		for(int i = 0; i<N-1; i++) {
-			for(int j = i+1; j<N; j++) {
-				if(arr[i] + arr[j] > 0) cnt++;
-			}
+		long cnt = 0;
+		Arrays.sort(arr, Comparator.reverseOrder());
+		for(int i = 0; i<N; i++) {
+			if(arr[i] <= 0) break;
+			cnt += find(arr, -arr[i]) - (i + 1); 
 		}
 		bw.write(cnt+"");
 		bw.flush();
